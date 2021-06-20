256445ce744b5eacf10994f4e16e10896318b471
dario <tacachojose@gmail.com>
2021-06-20 17:23:38 -0300

    eliminando archivos innecesarios

--- database/asociations.js
+++ database/asociations.js
@@ -2,7 +2,6 @@ const Genero = require("./models/Genero");
 const PeliculaSerie = require("./models/PeliculaSerie");
 const Personaje = require("./models/Personaje");
 
-//Probando
 Genero.belongsToMany(Personaje, {
   through: "genero_personaje",
   timestamps: false,
